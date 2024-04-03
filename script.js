1e4c246f286ccca97f22acda1997f71c6dfcdbce
MohammedMohammed2 <mohammed.mohammed1@gritacademy.se>
2024-04-03 08:48:30 +0200

    testing

---
+++ script.js
@@ -0,0 +1,23 @@
+const firstH2 = document.querySelector('h2');
+
+const allH2 = document.querySelectorAll('h2');
+console.log(allH2[1]);
+
+const allbtns=document.querySelectorAll('button');
+console.log(allbtns[1]);
+
+firstH2.innerText='xffing<h1>H1</h1>';
+allH2[1].innerHTML='xffing<h1>H1</h1>';
+
+const firstp=document.querySelector('p');
+firstp.style.backgroundColor='purple';
+
+const img = document.querySelector('img');
+img.src= "https://placebear.com/400/400";
+
+const navel= document.querySelector('nav');
+navel.classList.add('active');
+
+const classSelector=document.querySelector('.art-3');
+classSelector.style.backgroundColor='purple';
+
