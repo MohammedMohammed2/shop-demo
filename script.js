1cf142631ff66eed2f0b194fde13d301412bd04a
MohammedMohammed2 <mohammed.mohammed1@gritacademy.se>
2024-04-03 09:21:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,9 @@ navel.classList.add('active');
 const classSelector=document.querySelector('.art-3');
 classSelector.style.backgroundColor='purple';
 
+const allh3= document.querySelectorAll('h3');
+allh3[0].innerHTML='potato';
+
+const navchanger = document.querySelectorAll('nav');
+navchanger[0].innerText='start';
+
